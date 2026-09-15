327a1ff1026539f815d404073298c7e10364e5d2
Sargam Sahu <sargamsahu1011@gmail.com>
2026-09-15 15:28:37 +0530

    Prepare Flask backend and Vercel frontend deployment

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import "./App.css";
 
-const API_URL = "http://127.0.0.1:5000";
+const API_URL = "";
 
 function App() {
   const [jobText, setJobText] = useState("");
